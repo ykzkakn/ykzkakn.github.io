b24df6ae34eccd89749c61f99daca2bf133c9c69
ykzkakn <beninei4869@gmail.com>
2022-10-12 18:44:46 +0900

    2022javaScriptコミット

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
-console.log("ガチャを回すときは")
-console.log("夜中の3時とかがいいらしいとか")
-console.log("そういう都市伝説があるらしい")
+console.log("ガチャを回すときは");
+console.log("夜中の3時とかがいいらしいとか");
+console.log("そういう都市伝説があるらしい");
 
 let variableA;
 variableA="最初の代入A";
